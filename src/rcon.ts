8b2b75b0ca89a8f668d12e35e4c248e2640e4a48
enrate <80484441+enrate@users.noreply.github.com>
2025-05-01 08:54:00 +0300

    Update rcon.ts

--- src/rcon.ts
+++ src/rcon.ts
@@ -136,11 +136,11 @@ export class Rcon {
                     throw new Error('Игрок не найден в истории подключений');
                 }
                 if (playersRows.length > 1) {
-                    throw new Error(`Найдено несколько игроков с таким ником:\n${playersRows.map((value) => { return {'playerId': value.player_id, 'playerName': value.player_name }})}`)
+                    throw new Error(`Найдено несколько игроков с таким ником:\n${playersRows.map((value) => value.player_id).join('\n')}`)
                 }
                 targetPlayer = playersRows[0].player_id;
             } catch (error) {
-                throw new Error("Ошбка при поиске UID игрока по имени в БД")
+                throw new Error(`Ошибка при поиске UID игрока по имени в БД: ${error}`)
             }
         }
 
